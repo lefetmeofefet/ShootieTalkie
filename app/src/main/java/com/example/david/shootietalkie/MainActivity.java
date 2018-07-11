c2c592b3c75cbadd023dd40a97fc360f14257211
Jonny G <bashevsky@gmail.com>
2018-07-11 12:13:24 +0300

    Changed "Go" activity to start upon bootup.

--- app/src/main/java/com/example/david/shootietalkie/MainActivity.java
+++ app/src/main/java/com/example/david/shootietalkie/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.david.shootietalkie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -24,8 +25,11 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+
             }
         });
+        Intent go = new Intent(this , Go.class);
+        startActivity(go);
     }
 
     @Override
